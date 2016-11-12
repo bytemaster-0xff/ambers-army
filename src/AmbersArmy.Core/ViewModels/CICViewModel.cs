59dcaac81849724e0862c43c3184f85898680f5e
Kevin Wolf <kevinw@software-logistics.com>
2016-11-12 17:03:01 -0500

    can add amber alert from CIC

---
+++ src/AmbersArmy.Core/ViewModels/CICViewModel.cs
@@ -0,0 +1,50 @@
+﻿using AmbersArmy.Core.Services;
+using AmbersArmy.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AmbersArmy.Core.ViewModels
+{
+    public class CICViewModel
+    {
+   
+        FlowClient _flowClient = new FlowClient();
+
+        public CICViewModel()
+        {
+            Alert = new Models.Alert()
+            {
+                Name = "Amber",
+                Age = "12",
+                Weight = "80",
+                AlertType = Models.Alert.AlertTypes.Amber,
+                BirthDate = "June 15, 2012",
+                LastKnowLocationDateStamp = DateTime.Now.AddMinutes(-15).ToJSONString(),
+                LastKnownLocation = new Models.GeoLocation()
+                {
+                    Latitude = 33.745239,
+                    Longitude = -84.390151
+                },
+                LicensePlate = "ABC123",
+                VehicleColor = "White",
+                VehicleModel = "Ford Ranger",
+                VehicleYear = "2010",
+            };
+
+            AddAlertCommand = new RelayCommand(PostAlert);
+        }
+
+        public async void PostAlert()
+        {
+            await _flowClient.PostAlertAsync(Alert);
+        }
+
+        public Models.Alert Alert { get; private set; }
+
+        public ICommand AddAlertCommand { get; private set; }
+    }
+}
--- src/CIC/MainPage.xaml.cs
+++ src/CIC/MainPage.xaml.cs
@@ -13,6 +13,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using AmbersArmy.Core.Services;
+using System.Diagnostics;
+using Windows.Devices.Geolocation;
+using Windows.Services.Maps;
 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -24,6 +27,7 @@ namespace CIC
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int idx = 0;
         public MainPage()
         {
             this.InitializeComponent();
@@ -39,6 +43,54 @@ namespace CIC
 			var foo = result;
 			var fooCheck = string.IsNullOrWhiteSpace(result);
 			M2xResult.Text = result;
-		}
-	}
+
+            TheMap.Center = new Windows.Devices.Geolocation.Geopoint(new Windows.Devices.Geolocation.BasicGeoposition()
+            {
+               Latitude = 33.7490,
+               Longitude = -84.3880
+            });
+            TheMap.ZoomLevel = 14;
+            TheMap.Tapped += TheMap_Tapped;
+
+            TheMap.MapElementClick += TheMap_MapElementClick;
+            TheMap.Tapped += TheMap_Tapped1;
+
+            var startLocation = new BasicGeoposition() { Latitude = 33.717486, Longitude = -84.398336 };
+
+            // End at the city of Seattle, Washington.
+            var endLocation = new BasicGeoposition() { Latitude = 33.748577, Longitude = -84.441310 };
+
+            // Get the route between the points.
+            var  routeResult =
+                  await MapRouteFinder.GetDrivingRouteAsync(
+                  new Geopoint(startLocation),
+                  new Geopoint(endLocation),
+                  MapRouteOptimization.Time,
+                  MapRouteRestrictions.None);
+
+            foreach(var item in routeResult.Route.Legs)
+            {
+                foreach(var path in item.Path.Positions)
+                {
+                    Debug.WriteLine($"{{new SampleGeoPos() {{Idx ={idx++},Lat={path.Latitude},Lon={path.Longitude}}}}},");
+                }
+            }
+
+        }
+
+        private void TheMap_Tapped1(object sender, TappedRoutedEventArgs e)
+        {
+           
+        }
+
+        private void TheMap_MapElementClick(Windows.UI.Xaml.Controls.Maps.MapControl sender, Windows.UI.Xaml.Controls.Maps.MapElementClickEventArgs args)
+        {
+            Debug.WriteLine($"{{new SampleGeoPos() {{Idx ={idx++},Lat={args.Location.Position.Latitude},Lon={args.Location.Position.Longitude}}}}}}}"); 
+        }
+
+        private void TheMap_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
