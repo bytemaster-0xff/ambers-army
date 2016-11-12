22ae6bf368026a1805d3222f9a601ed7d3ffc949
Kevin Wolf <kevinw@software-logistics.com>
2016-11-12 14:01:14 -0500

    added reader to take pix to UWP lib

---
+++ src/AmbersArmy.UWP.Common/LicensePlateRecog/Reader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.Storage.Streams;
+
+namespace AmbersArmy.UWP.Common.LicensePlateRecog
+{
+    public class Reader
+    {
+        MediaCapture _mediaCapture;
+
+        public Reader()
+        {
+            _mediaCapture = new MediaCapture();
+        }
+
+        public async Task InitAsync()
+        {
+            _mediaCapture = new MediaCapture();
+            await _mediaCapture.InitializeAsync();
+            _mediaCapture.Failed += _mediaCapture_Failed;
+        }
+
+        public async void TakePix()
+        {
+            var myPictures = await Windows.Storage.StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Pictures);
+            var file = await myPictures.SaveFolder.CreateFileAsync("photo.jpg", CreationCollisionOption.GenerateUniqueName);
+
+            using (var captureStream = new InMemoryRandomAccessStream())
+            {
+                await _mediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateJpeg(), captureStream);
+
+                using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    var decoder = await BitmapDecoder.CreateAsync(captureStream);
+                    var encoder = await BitmapEncoder.CreateForTranscodingAsync(fileStream, decoder);
+
+                    var properties = new BitmapPropertySet
+                    {
+                        {
+                            "System.Photo.Orientation",
+                            new BitmapTypedValue(PhotoOrientation.Normal, PropertyType.UInt16)
+                        }
+                    };
+
+                    await encoder.BitmapProperties.SetPropertiesAsync(properties);
+                    await encoder.FlushAsync();
+                }
+            }
+        }
+
+        private void _mediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
