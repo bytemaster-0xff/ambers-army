ddb9a6fa127930f94496a6a44348145e9c7eaae5
Kevin Wolf <kevinw@software-logistics.com>
2016-11-12 13:39:36 -0500

    Flow client now posting (limited) data

--- src/AmbersArmy.Core/Services/FlowClient.cs
+++ src/AmbersArmy.Core/Services/FlowClient.cs
@@ -11,21 +11,21 @@ namespace AmbersArmy.Core.Services
 
         public Task PostLicensePlateAysnc(FoundPlate foundPlate)
         {
-            var uri = "https://run-west.att.io/3f7a4e0dfd560/257e916b1900/6a423dcbc2f47b1/in/flow/plates";
+            var uri = "https://run-west.att.io/3f7a4e0dfd560/257e916b1900/6a423dcbc2f47b1/in/flow/licenseplates/add";
             var json = JsonConvert.SerializeObject(foundPlate);
             return _client.PostAsync(uri, new StringContent(json));
         }
 
         public  Task PostLocationAsync(Models.Location location)
         {
-            var uri = "https://run-west.att.io/3f7a4e0dfd560/257e916b1900/6a423dcbc2f47b1/in/flow/location";
+            var uri = "https://run-west.att.io/3f7a4e0dfd560/257e916b1900/6a423dcbc2f47b1/in/flow/location/add";
             var json = JsonConvert.SerializeObject(location);
             return _client.PostAsync(uri, new StringContent(json));
         }
 
         public  Task PostAlertAsync(Models.Alert alert)
         {
-            var uri = "https://run-west.att.io/3f7a4e0dfd560/257e916b1900/6a423dcbc2f47b1/in/flow/ingestalert";
+            var uri = "https://run-west.att.io/3f7a4e0dfd560/257e916b1900/6a423dcbc2f47b1/in/flow/alerts/add";
             var json = JsonConvert.SerializeObject(alert);
             return _client.PostAsync(uri, new StringContent(json));
         }
