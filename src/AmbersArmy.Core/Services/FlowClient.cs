ae3d8920dc86b8175cebb8c1f944d91c6bdd27ee
Kevin Wolf <kevinw@software-logistics.com>
2016-11-13 10:46:33 -0500

    added support for notifications

--- src/AmbersArmy.Core/Services/FlowClient.cs
+++ src/AmbersArmy.Core/Services/FlowClient.cs
@@ -25,7 +25,7 @@ namespace AmbersArmy.Core.Services
 
         public  Task PostAlertAsync(Models.Alert alert)
         {
-            var uri = "https://run-west.att.io/3f7a4e0dfd560/257e916b1900/6a423dcbc2f47b1/in/flow/alerts/add";
+            var uri = "https://run-west.att.io/0a5232880e7f2/c0c49b19717e/8b8fac529ecc277/in/flow/alerts/add";
             var json = JsonConvert.SerializeObject(alert);
             return _client.PostAsync(uri, new StringContent(json));
         }
