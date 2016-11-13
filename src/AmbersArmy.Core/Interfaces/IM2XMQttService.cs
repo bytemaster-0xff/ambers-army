a12014b75558fe0d85fc30c43608cb1906d448ef
Kelvin McDaniel <kelvin.mcdaniel@live.com>
2016-11-12 20:50:31 -0500

    Got subscription M2X working.

---
+++ src/AmbersArmy.Core/Interfaces/IM2XMQttService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AmbersArmy.Core.Interfaces
+{
+	public interface IM2XMqttService
+	{
+		Task PostStreamValues(string deviceId, string streamName, string values);
+		Task PostStreamValues<T>(string deviceId, string streamName, List<T> values);
+	}
+}
--- src/AmbersArmy.Core/ViewModels/Spotter.cs
+++ src/AmbersArmy.Core/ViewModels/Spotter.cs
@@ -2,6 +2,7 @@
 using AmbersArmy.Core.Models;
 using AmbersArmy.Core.Services;
 using AmbersArmy.Core.Utils;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -13,69 +14,91 @@ using System.Windows.Input;
 
 namespace AmbersArmy.Core.ViewModels
 {
-    public class Spotter
-    {
-        FlowClient _flowClient = new FlowClient();
-        Interfaces.ILicensePlateReader _plateReader;
-        Interfaces.ITimer _timer;
-        private ObservableCollection<String> _recognizedTags;
+	public class Spotter
+	{
+		FlowClient _flowClient = new FlowClient();
+		Interfaces.ILicensePlateReader _plateReader;
+		Interfaces.ITimer _timer;
+		Interfaces.IM2XMqttService _m2xMqttService;
+		private ObservableCollection<String> _recognizedTags;
 
-        public Spotter()
-        {
-            PostLocationCommand = new RelayCommand(PostLocation);
-            CurrentLocation = new GeoLocation()
-            {
-                Latitude = 36.99,
-                Longitude = -86.0
-            };
+		public Spotter()
+		{
+			PostLocationCommand = new RelayCommand(PostLocation);
+			PostLicensePlateCommand = new RelayCommand(PostLicensePlate);
+			CurrentLocation = new GeoLocation()
+			{
+				Latitude = 36.99,
+				Longitude = -86.0
+			};
 
-           
-            _plateReader = AAIOC.Get<Interfaces.ILicensePlateReader>();
-            _plateReader.TextRecognized += _plateReader_TextRecognized;
-            _recognizedTags = new ObservableCollection<string>();
-        }
 
-        public async Task InitAsync()
-        {
-            Debug.WriteLine("INITTING READER");
-            await _plateReader.InitAsync();
-            _timer = AAIOC.Get<ITimerFactory>().CreateTimer(TimeSpan.FromSeconds(1));
-            _timer.Elapsed += _timer_Elapsed;
-            _timer.Start();
-            Debug.WriteLine("WE INIT SO START TIMER!");
-        }
+			_plateReader = AAIOC.Get<Interfaces.ILicensePlateReader>();
+			_plateReader.TextRecognized += _plateReader_TextRecognized;
+			_recognizedTags = new ObservableCollection<string>();
 
-        private void _plateReader_TextRecognized(object sender, OCRResult e)
-        {
-            _recognizedTags.Add(e.AllText);
-        }
+			_m2xMqttService = AAIOC.Get<Interfaces.IM2XMqttService>();
+		}
 
-        private async void _timer_Elapsed(object sender, EventArgs e)
-        {
-            _timer.Stop();
-             await _plateReader.ScanNow();
-            _timer.Start();
-        }
+		public async Task InitAsync()
+		{
+			Debug.WriteLine("INITTING READER");
+			await _plateReader.InitAsync();
+			_timer = AAIOC.Get<ITimerFactory>().CreateTimer(TimeSpan.FromSeconds(1));
+			_timer.Elapsed += _timer_Elapsed;
+			_timer.Start();
+			Debug.WriteLine("WE INIT SO START TIMER!");
+		}
 
-        public GeoLocation CurrentLocation { get; set; }
+		private void _plateReader_TextRecognized(object sender, OCRResult e)
+		{
+			_recognizedTags.Add(e.AllText);
+		}
 
-        public String SpotterId { get; set; }
+		private async void _timer_Elapsed(object sender, EventArgs e)
+		{
+			_timer.Stop();
+			await _plateReader.ScanNow();
+			_timer.Start();
+		}
 
-        public async void PostLocation()
-        {
-            var location = new Models.Location()
-            {
-                DeviceId = SpotterId,
-                Type = Location.LocationType.Spotter,
-                Geo = CurrentLocation,
-                TimeStamp = DateTime.Now.ToJSONString()
-            };
+		public GeoLocation CurrentLocation { get; set; }
 
-            await _flowClient.PostLocationAsync(location);
-        }
+		public String SpotterId { get; set; }
 
-        public ICommand PostLocationCommand { get; private set; }
+		public async void PostLocation()
+		{
+			var jsonPayload = JsonConvert.SerializeObject(new Models.Location()
+			{
+				DeviceId = SpotterId,
+				Type = Location.LocationType.Spotter,
+				Geo = CurrentLocation,
+				TimeStamp = DateTime.Now.ToJSONString()
+			});
 
-        public ObservableCollection<string> RecognizedTags { get { return _recognizedTags; } }
-    }
+			var timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:00Z");
+			var postParms = $"{{ \"values\": [ {{ \"timestamp\":\"{timestamp}\", \"value\": \"{jsonPayload.Replace("\"", "&quot;")}\" }} ] }}";
+			await _m2xMqttService.PostStreamValues("6e7bb7923219c6b72728cb1a34d0d5b6", "locations", postParms);
+		}
+
+		public async void PostLicensePlate()
+		{
+			var jsonPayload = JsonConvert.SerializeObject(new Models.FoundPlate()
+			{
+				Location = new GeoLocation { Latitude = 39, Longitude = -82 },
+				Plate = "E61 5WS",
+				TimeStamp = DateTime.Now.ToJSONString()
+			});
+
+			var timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:00Z");
+			var postParms = $"{{ \"values\": [ {{ \"timestamp\":\"{timestamp}\", \"value\": \"{jsonPayload.Replace("\"", "&quot;")}\" }} ] }}";
+			await _m2xMqttService.PostStreamValues("6e7bb7923219c6b72728cb1a34d0d5b6", "licenseplate", postParms);
+		}
+
+		public ICommand PostLocationCommand { get; private set; }
+
+		public ICommand PostLicensePlateCommand { get; private set; }
+
+		public ObservableCollection<string> RecognizedTags { get { return _recognizedTags; } }
+	}
 }
